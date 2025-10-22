ed312cb20aa5d4c580f34a837abbaf382576242c
jeffyevangelista <jevangelista@classify.com.ph>
2025-10-22 08:48:36 +0800

    feat: add keyboard controller provider with gesture handler support

--- providers/RootProvider.tsx
+++ providers/RootProvider.tsx
@@ -1,5 +1,11 @@
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import KeyboardProvider from "./KeyboardProvider";
 import QueryProvider from "./QueryProvider";
 
 export default ({ children }: { children: React.ReactNode }) => (
-  <QueryProvider>{children}</QueryProvider>
+  <GestureHandlerRootView>
+    <KeyboardProvider>
+      <QueryProvider>{children}</QueryProvider>
+    </KeyboardProvider>
+  </GestureHandlerRootView>
 );
