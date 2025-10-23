121e10515107c7cd5bf5c53e35dacaeb8f015c57
jeffyevangelista <jevangelista@classify.com.ph>
2025-10-23 15:39:00 +0800

    refactor: move login form layout and branding to LoginScreen component

--- features/auth/components/LoginForm.tsx
+++ features/auth/components/LoginForm.tsx
@@ -1,4 +1,3 @@
-import AppLogo from "@/assets/images/app-logo.svg";
 import { Alert, AlertIcon, AlertText } from "@/components/ui/alert";
 import { Box } from "@/components/ui/box";
 import { Button, ButtonText } from "@/components/ui/button";
@@ -9,9 +8,7 @@ import {
   FormControlLabel,
   FormControlLabelText,
 } from "@/components/ui/form-control";
-import { Heading } from "@/components/ui/heading";
 import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
-import { Text } from "@/components/ui/text";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Link } from "expo-router";
 import { useState } from "react";
@@ -20,13 +17,11 @@ import { useWindowDimensions } from "react-native";
 import { EyeIcon, EyeSlashIcon } from "react-native-heroicons/outline";
 import { useLogin } from "../auth.hooks";
 import { LoginFormValues, loginSchema } from "../auth.schemas";
-import MSAuthButton from "./MSAuthButton";
+
 const LoginForm = () => {
-  const { height } = useWindowDimensions();
-  const verticalPadding = height > 800 ? 64 : 30;
   const [showPassword, setShowPassword] = useState(false);
   const { mutateAsync: login, isPending, isError, error } = useLogin();
-
+  const { height } = useWindowDimensions();
   const {
     control,
     handleSubmit,
@@ -43,110 +38,87 @@ const LoginForm = () => {
   };
 
   return (
-    <Box
-      style={{ paddingTop: verticalPadding }}
-      className={`flex-1 items-center justify-start px-6`}
-    >
-      <Box className="w-full max-w-md">
-        <Box className="items-center mb-10">
-          <AppLogo width={112} height={112} style={{ marginBottom: 6 }} />
-          <Heading className="text-2xl font-semibold mb-1 text-center">
-            Welcome to Classedge
-          </Heading>
-          <Text className="text-gray-500 text-center">
-            A learning platform of HCCCI
-          </Text>
-        </Box>
+    <Box className="w-full gap-5">
+      {isError && (
+        <Alert action="error">
+          <AlertIcon />
+          <AlertText>{error.message}</AlertText>
+        </Alert>
+      )}
 
-        <Box className="w-full gap-5">
-          {isError && (
-            <Alert action="error">
-              <AlertIcon />
-              <AlertText>{error.message}</AlertText>
-            </Alert>
+      <FormControl isInvalid={!!errors.username}>
+        <FormControlLabel>
+          <FormControlLabelText>Email</FormControlLabelText>
+        </FormControlLabel>
+        <Controller
+          control={control}
+          name="username"
+          render={({ field: { onChange, value } }) => (
+            <Input size={height > 800 ? "xl" : "lg"}>
+              <InputField
+                keyboardType="email-address"
+                autoCapitalize="none"
+                placeholder="juandelacruz@hccci.edu.ph"
+                value={value}
+                onChangeText={onChange}
+              />
+            </Input>
           )}
+        />
+        {errors.username && (
+          <FormControlError>
+            <FormControlErrorText>
+              {errors.username.message}
+            </FormControlErrorText>
+          </FormControlError>
+        )}
+      </FormControl>
 
-          <FormControl isInvalid={!!errors.username}>
-            <FormControlLabel>
-              <FormControlLabelText>Email</FormControlLabelText>
-            </FormControlLabel>
-            <Controller
-              control={control}
-              name="username"
-              render={({ field: { onChange, value } }) => (
-                <Input size={height > 800 ? "xl" : "lg"}>
-                  <InputField
-                    keyboardType="email-address"
-                    autoCapitalize="none"
-                    placeholder="juandelacruz@hccci.edu.ph"
-                    value={value}
-                    onChangeText={onChange}
-                  />
-                </Input>
-              )}
-            />
-            {errors.username && (
-              <FormControlError>
-                <FormControlErrorText>
-                  {errors.username.message}
-                </FormControlErrorText>
-              </FormControlError>
-            )}
-          </FormControl>
-
-          <FormControl isInvalid={!!errors.password}>
-            <FormControlLabel>
-              <FormControlLabelText>Password</FormControlLabelText>
-            </FormControlLabel>
-            <Controller
-              control={control}
-              name="password"
-              render={({ field: { onChange, value } }) => (
-                <Input size={height > 800 ? "xl" : "lg"}>
-                  <InputField
-                    value={value}
-                    onChangeText={onChange}
-                    secureTextEntry={!showPassword}
-                  />
-                  <InputSlot
-                    className="pr-3"
-                    onPress={() => setShowPassword((s) => !s)}
-                  >
-                    <InputIcon as={showPassword ? EyeIcon : EyeSlashIcon} />
-                  </InputSlot>
-                </Input>
-              )}
-            />
-            {errors.password && (
-              <FormControlError>
-                <FormControlErrorText>
-                  {errors.password.message}
-                </FormControlErrorText>
-              </FormControlError>
-            )}
-            <Link href="/forgot-password" className="self-end mt-2" asChild>
-              <Button variant="link">
-                <ButtonText>Forgot Password?</ButtonText>
-              </Button>
-            </Link>
-          </FormControl>
-
-          <Button
-            size={height > 800 ? "xl" : "lg"}
-            onPress={handleSubmit(handleLogin)}
-            isDisabled={isPending}
-            className="mt-2"
-          >
-            <ButtonText>Login</ButtonText>
+      <FormControl isInvalid={!!errors.password}>
+        <FormControlLabel>
+          <FormControlLabelText>Password</FormControlLabelText>
+        </FormControlLabel>
+        <Controller
+          control={control}
+          name="password"
+          render={({ field: { onChange, value } }) => (
+            <Input size={height > 800 ? "xl" : "lg"}>
+              <InputField
+                value={value}
+                onChangeText={onChange}
+                secureTextEntry={!showPassword}
+              />
+              <InputSlot
+                className="pr-3"
+                onPress={() => setShowPassword((s) => !s)}
+              >
+                <InputIcon as={showPassword ? EyeIcon : EyeSlashIcon} />
+              </InputSlot>
+            </Input>
+          )}
+        />
+        {errors.password && (
+          <FormControlError>
+            <FormControlErrorText>
+              {errors.password.message}
+            </FormControlErrorText>
+          </FormControlError>
+        )}
+        <Link href="/forgot-password" className="self-end mt-2" asChild>
+          <Button variant="link">
+            <ButtonText>Forgot Password?</ButtonText>
           </Button>
-        </Box>
+        </Link>
+      </FormControl>
 
-        {/* Footer Section */}
-        <Box className="items-center gap-3 mt-10">
-          <Text className="text-gray-500">or continue with</Text>
-          <MSAuthButton />
-        </Box>
-      </Box>
+      <Button
+        size={height > 800 ? "xl" : "lg"}
+        onPress={handleSubmit(handleLogin)}
+        isDisabled={isPending}
+        className="mt-2"
+      >
+        <ButtonText>Login</ButtonText>
+      </Button>
     </Box>
   );
 };
--- screens/auth/LoginScreen.tsx
+++ screens/auth/LoginScreen.tsx
@@ -1,7 +1,16 @@
+import AppLogo from "@/assets/images/app-logo.svg";
+import { Box } from "@/components/ui/box";
+import { Heading } from "@/components/ui/heading";
+import { Text } from "@/components/ui/text";
 import LoginForm from "@/features/auth/components/LoginForm";
+import MSAuthButton from "@/features/auth/components/MSAuthButton";
+import { useWindowDimensions } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-controller";
 
 export default () => {
+  const { height } = useWindowDimensions();
+  const verticalPadding = height > 800 ? 64 : 30;
+
   return (
     <KeyboardAwareScrollView
       showsVerticalScrollIndicator={false}
@@ -9,7 +18,30 @@ export default () => {
       keyboardShouldPersistTaps="handled"
       contentContainerStyle={{ flexGrow: 1, backgroundColor: "#f9f9f9" }}
     >
-      <LoginForm />
+      <Box
+        style={{ paddingTop: verticalPadding }}
+        className={`flex-1 items-center justify-start px-6`}
+      >
+        <Box className="w-full max-w-md">
+          <Box className="items-center mb-10">
+            <AppLogo width={112} height={112} style={{ marginBottom: 6 }} />
+            <Heading className="text-2xl font-semibold mb-1 text-center">
+              Welcome to Classedge
+            </Heading>
+            <Text className="text-gray-500 text-center">
+              A learning platform of HCCCI
+            </Text>
+          </Box>
+
+          <LoginForm />
+
+          {/* Footer Section */}
+          <Box className="items-center gap-3 mt-10">
+            <Text className="text-gray-500">or continue with</Text>
+            <MSAuthButton />
+          </Box>
+        </Box>
+      </Box>
     </KeyboardAwareScrollView>
   );
 };
